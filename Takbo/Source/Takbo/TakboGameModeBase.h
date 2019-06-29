48d8b848d5dafb1e5b58271eaa35dc7abf28a58d
oO-Blink-Oo <alvarezian23@gmail.com>
2019-06-28 23:42:45 -0700

    real initial commit

---
+++ Takbo/Source/Takbo/TakboGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TakboGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAKBO_API ATakboGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
