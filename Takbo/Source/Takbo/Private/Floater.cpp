2e205dc0c4b900bda7d5a8bb3815d7eb5ad22fcd
oO-Blink-Oo <alvarezian23@gmail.com>
2019-07-23 15:02:35 -0700

    testing initial movements

---
+++ Takbo/Source/Takbo/Private/Floater.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Floater.h"
+
+// Sets default values
+AFloater::AFloater()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(FName("MeshComponent"));
+	SetRootComponent(MeshComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(FName("Camera"));
+	Camera->AttachTo(MeshComponent);
+	Camera->SetRelativeLocation(FVector(-300.f, 0.f, 300.f));
+	Camera->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	MaxSpeed = 100.f;
+	CurrentVelocity = FVector(0.f);
+}
+
+// Called when the game starts or when spawned
+void AFloater::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloater::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+	SetActorLocation(NewLocation);
+}
+
+// Called to bind functionality to input
+void AFloater::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &AFloater::MoveForward);
+	PlayerInputComponent->BindAxis(FName("MoveRight"), this, &AFloater::MoveRight);
+}
+
+
+void AFloater::MoveForward(float Value)
+{
+	CurrentVelocity.X = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+}
+
+void AFloater::MoveRight(float Value)
+{
+	CurrentVelocity.Y = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+}
