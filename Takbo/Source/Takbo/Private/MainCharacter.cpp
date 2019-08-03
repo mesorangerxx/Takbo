3238040be90853b7d61189257bf5972d0037b798
oO-Blink-Oo <alvarezian23@gmail.com>
2019-08-02 23:52:33 -0700

    created initial character actor

---
+++ Takbo/Source/Takbo/Private/MainCharacter.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainCharacter.h"
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CharacterMaxMovementSpeed = 200.f;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(FName("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 900.f;
+	CameraBoom->RelativeRotation = FRotator(-45.f, 0.f, 0.f);
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(FName("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->RelativeLocation = FVector(106.f, 0.f, 106.f);
+}
+
+// Called when the game starts or when spawned
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMainCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMainCharacter::MoveRight);
+}
+
+
+void AMainCharacter::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.f)) // This means if that there is a controller and Value is not 0.f then that means you could do something to move forward
+	{
+		FVector Direction = FVector(1.f, 0.f, 0.f);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMainCharacter::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.f)) // This means if that there is a controller and Value is not 0.f then that means you could do something to move forward
+	{
+		FVector Direction = FVector(0.f, 1.f, 0.f);
+		AddMovementInput(Direction, Value);
+	}
+}
---
+++ Takbo/Source/Takbo/Public/MainCharacter.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "MainCharacter.generated.h"
+
+UCLASS()
+class TAKBO_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMainCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Character Maximum Speed")
+	float CharacterMaxMovementSpeed;
+
+	/** This will allow us to pick a mesh that the character will have*/
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MeshComponent; // This can be changed to a skeletal mesh later
+
+	/** Camera boom positioning the camera behind the player */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleDefaultsOnly)
+	UCameraComponent* FollowCamera;
+
+	/** These are just getters for the SpringArm and Camera*/
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	FVector CurrentVelocity;
+};
